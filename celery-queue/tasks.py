5ad668a0363e807cf79ffbe4a587d5435ce5f3a1
root <root@debian>
2019-03-20 13:27:01 +0100

    check/next_prime as nested functions

--- celery-queue/tasks.py
+++ celery-queue/tasks.py
@@ -11,6 +11,28 @@ celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND
 
 @celery.task(name='tasks.factors')
 def factors(n: int) -> list:
+
+    # Returns true if n is a prime number
+    def check_prime(n):
+        x = 2
+        while(x < n):
+            if n % x == 0:
+                # Factor other then 1 or n, number is composite
+                return False
+            x = x +1
+        # Number is prime, while loop terminated without finding factor
+        return True
+    
+    # Finds the next prime after n
+    def next_prime(n):
+        val = n + 1
+        while(True):
+            # Check if val (n + 1) is a prime
+            if check_prime(val):
+                return val
+            else:
+                val = val + 1
+    
     factor_list = []
     prime = 1
     while (next_prime(prime) <= n):
@@ -19,25 +41,4 @@ def factors(n: int) -> list:
             factor_list.append(next_prime(prime))
         prime = next_prime(prime)
     return factor_list
-
-# Returns true if n is a prime number
-def check_prime(n):
-    x = 2
-    while(x < n):
-        if n % x == 0:
-            # Factor other then 1 or n, number is composite
-            return False
-        x = x +1
-    # Number is prime, while loop terminated without finding factor
-    return True
-
-# Finds the next prime after n
-def next_prime(n):
-    val = n + 1
-    while(True):
-        # Check if val (n + 1) is a prime
-        if check_prime(val):
-            return val
-        else:
-            val = val + 1
 
