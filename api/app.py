a04e91c195ae944d3be1a6154fb69a4883861b20
root <root@debian>
2019-03-20 11:35:08 +0100

    update API

--- api/app.py
+++ api/app.py
@@ -5,17 +5,11 @@ import celery.states as states
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return "Hello world!"
-
-@app.route('/add/<int:param1>/<int:param2>')
-def add(param1: int, param2: int) -> str:
-    task = celery.send_task('tasks.add', args=[param1, param2], kwargs={})
+@app.route('/factors/<int:param1>')
+def add(param1: int) -> str:
+    task = celery.send_task('tasks.add', args=[param1], kwargs={})
     response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
     return response
-
 
 @app.route('/check/<string:task_id>')
 def check_task(task_id: str) -> str:
